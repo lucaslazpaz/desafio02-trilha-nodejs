52c731b8a8f042c4cc5ca3c090511f0895e605e5
lucaslazpaz <lucaslazarofatec@gmail.com>
2021-07-23 09:03:51 -0300

    [add] resolution

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const user = {
+        user_id: <string>request.headers.user_id,
+      };
+
+      const users = this.listAllUsersUseCase.execute({ user_id: user.user_id });
+
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,11 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const findUser = this.usersRepository.findById(user_id);
+
+    const user = this.usersRepository.turnAdmin(findUser);
+
+    return user;
   }
 }
 
